6d3e242fb26d12d8412de67d7d52feac8cefd6f6
Ganesh <ganeshpendyala0314@gmail.com>
2024-02-04 16:42:18 +0530

    first commit

---
+++ Calculator/index.js
@@ -0,0 +1,23 @@
+let display = document.querySelector('.display');
+console.log(display);
+
+// Function  to add the user input to the display screen
+function addTodisplay(input){
+    display.textContent+=input;
+}
+
+// Clears the Display screen
+document.querySelector('#clear').addEventListener('click',function(){
+    display.textContent = " ";
+});
+
+// function to calculate the final result of the expression that is entered by the user
+function calculate(){
+    try{
+        display.textContent= eval(display.textContent);
+        console.log(display.textContent);
+    }
+    catch(error){
+        display.value= "ERROR";
+    }
+}
